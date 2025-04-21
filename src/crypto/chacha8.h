7d62fa1f3440e829cb22fdcee628a466f9cacad9
zhanghao <33890982+xk320@users.noreply.github.com>
2025-04-21 22:58:15 +0800

    Update chacha8.h

--- src/crypto/chacha8.h
+++ src/crypto/chacha8.h
@@ -11,7 +11,7 @@
 #include <string>
 
 #include "hash.h"
-
+#include <algorithm>
 namespace Crypto {
   extern "C" {
 #endif
@@ -45,7 +45,7 @@ namespace Crypto {
     static_assert(sizeof(chacha8_key) <= sizeof(Hash), "Size of hash must be at least that of chacha8_key");
     Hash pwd_hash;
     cn_slow_hash(context, password.data(), password.size(), pwd_hash);
-    memcpy(&key, &pwd_hash, sizeof(key));
+    std::copy_n(pwd_hash.data, sizeof(key.data), key.data);
     memset(&pwd_hash, 0, sizeof(pwd_hash));
   }
 }
